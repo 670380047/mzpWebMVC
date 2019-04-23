22245bd9ed1299d2318729b02544b47b8b271586
mzp <670380047@qq.com>
2019-04-23 11:28:21 +0800

    springboot

--- src/main/java/com/example/cron/CronTest.java
+++ src/main/java/com/example/cron/CronTest.java
@@ -1,4 +1,4 @@
-package util.cron;/**
+package com.example.cron;/**
  * @version: java version 1.7+
  * @Author : mzp
  * @Time : 2019/4/19 16:39
@@ -6,6 +6,7 @@ package util.cron;/**
  * @Software: IntelliJ IDEA 2019.3.15
  */
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -17,10 +18,11 @@ import java.util.Date;
  * @Description:
  * @Date: 2019/4/19 16:39
  */
-@Service
+@Service("cronService")
 public class CronTest {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    @Scheduled(cron = "*/5 * * * * ?")
     public void helloCron(){
-        System.out.println("时间："+sdf.format(new Date()));
+        System.out.println("定时器任务时间："+sdf.format(new Date()));
     }
 }
